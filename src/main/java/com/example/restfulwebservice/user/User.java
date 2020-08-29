fbe0408fbb7be9f339f67868499f122111572303
devTaehyeok <ith13579@korea.ac.kr>
2020-08-29 23:00:16 +0900

    Swagger.UI description 추가

--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -1,31 +1,35 @@
 package com.example.restfulwebservice.user;
 
-import com.fasterxml.jackson.annotation.JsonFilter;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Date;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
+import java.util.Date;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ApiModel(description = "사용자 상세 정보를 위한 도메인 객체")
 //@JsonIgnoreProperties(value={"password"})
 // @JsonFilter("UserInfo")
 public class User {
     private Integer id;
     // message 없으면 default message
     @Size(min = 2, message = "Name은 2글자 이상 입력해 주세요")
+    @ApiModelProperty(notes = "사용자 이름을 입력해 주세요")
     private String name;
     @Past
+    @ApiModelProperty(notes = "사용자 등록일을 입력해 주세요")
     private Date joinDate;
-//    @JsonIgnore
+    //    @JsonIgnore
+    @ApiModelProperty(notes = "사용자 비밀번호를 입력해 주세요")
     private String password;
-//    @JsonIgnore
+    //    @JsonIgnore
+    @ApiModelProperty(notes = "사용자 주빈번호를 입력해 주세요")
     private String ssn;
 
 }
